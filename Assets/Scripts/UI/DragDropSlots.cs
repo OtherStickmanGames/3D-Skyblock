7c731dd30dd72c030adbd4ff6f06bed734df00ad
OtherStickmanGames <other.stickman.games@gmail.com>
2023-11-27 00:14:30 +0300

    Таскание итемов

--- Assets/Scripts/UI/DragDropSlots.cs
+++ Assets/Scripts/UI/DragDropSlots.cs
@@ -7,8 +7,11 @@ using System.Linq;
 
 public class DragDropSlots : MonoBehaviour
 {
+    SlotView startSlot;
     InventoryItem dragable;
     Vector3 itemHolderOriginPos;
+    Transform originParent;
+
 
     private void Update()
     {
@@ -25,23 +28,62 @@ public class DragDropSlots : MonoBehaviour
                 //print(slot);
                 if (slot && slot.Item != null)
                 {
+                    originParent = slot.transform;
+                    startSlot = slot;
                     itemHolderOriginPos = slot.Item.view.transform.parent.localPosition;
                     dragable = slot.Item;
+                    dragable.view.transform.parent.SetParent(transform, false);
                 }
             }
         }
 
         if(dragable != null)
         {
-            var ScreenScale = transform.root.lossyScale.x;
-            float k = transform.lossyScale.x / ScreenScale;
-            var t = dragable.view.transform.parent;
-            float x = (Input.mousePosition.x * k);// - ((Screen.width / 2) * k);
-            float y = (Input.mousePosition.y * k);// - ((Screen.height / 2) * k);
-            float z = t.position.z;
-            t.position = new Vector3(x, y, z);
+            var scaleFactor = (float)1600 / (float)Screen.width;
+            var mousePos = Input.mousePosition;
+            Vector2 anchoredPos = (mousePos - new Vector3(Screen.width / 2, Screen.height / 2)) * scaleFactor;
+            var uiHolder = dragable.view.transform.parent as RectTransform;
+            //anchoredPos -= uiHolder.sizeDelta / 2;
+            uiHolder.anchoredPosition = anchoredPos;
+
+            //var ScreenScale = transform.root.lossyScale.x;
+            //var ebobob = dragable.view.transform.parent.parent.parent.lossyScale.x;
+            //float k = ebobob;//transform.lossyScale.x / ScreenScale;
+            //var t = dragable.view.transform.parent;
+            //float x = (Input.mousePosition.x * k);// - ((Screen.width / 2) * k);
+            //float y = (Input.mousePosition.y * k);// - ((Screen.height / 2) * k);
+            //float z = t.position.z;
+            //t.position = new Vector3(x, y, z);
         }
 
+        if (Input.GetMouseButtonUp(0))
+        {
+            if(dragable != null)
+            {
+                dragable.view.transform.parent.localPosition = itemHolderOriginPos;
+                dragable.view.transform.parent.SetParent(originParent, false);
 
+                PointerEventData ped = new(EventSystem.current) { position = Input.mousePosition };
+                List<RaycastResult> hits = new();
+                EventSystem.current.RaycastAll(ped, hits);
+
+                foreach (var hit in hits)
+                {
+                    if (hit.gameObject)
+                    {
+                        var slot = hit.gameObject.GetComponent<SlotView>();
+                        if(slot && slot != startSlot)
+                        {
+                            slot.SetItem(dragable);
+                            startSlot.RemoveItem();
+                        }
+                    }
+                }
+
+                dragable = null;
+                startSlot = null;
+                originParent = null;
+            }
+        }
     }
 }
